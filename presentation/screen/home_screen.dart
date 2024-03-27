3ca761926538e8c08dbfc0c0e5a89dec324a6bbb
feliphearaujo <feliphearaujoti@gmail.com>
2024-03-27 19:55:36 -0300

    Iniciando a construção do menu

---
+++ presentation/screen/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+  // Define o item selecionado no menu drawer
+  int _selectedIndex = 0;
+
+  HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Theme(
+      data: ThemeData.light(),
+      child: Scaffold(
+
+// Titulo
+        appBar: AppBar(
+          title: const Text('FoodExpress'),
+        ),
+        
+        drawer: _drawer(context),
+
+// Corpo
+        body: Container(),
+
+      ),
+    );
+
+  }
+  
+///Método responsável para criar o menu
+///drawer da aplicação
+  Widget _drawer(BuildContext context) {
+    return ListView(
+    children: [
+      // cabeçalho
+      const DrawerHeader(child: Text("Home")),
+
+      // componente usuário
+      ListTile(
+        title: const Text('Usuário'),
+        selected: _selectedIndex == 0,
+        leading: const Icon(Icons.people),
+        onTap: () {
+          // Fecha o menu ao clicar
+          Navigator.pop(context);
+        },
+      )
+    ],
+
+  );
+  }
+  
+}
